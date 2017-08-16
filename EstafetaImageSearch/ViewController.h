cceaad8f001a87c43fc7f59334be1f000e66b129
jPavel <jpavel.ua@gmail.com>
2017-08-16 07:01:09 +0300

    Initial Commit

---
+++ EstafetaImageSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EstafetaImageSearch
+//
+//  Created by Pavel Kozlovsky on 8/16/17.
+//  Copyright © 2017 Pavel Kozlovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EstafetaImageSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EstafetaImageSearch
+//
+//  Created by Pavel Kozlovsky on 8/16/17.
+//  Copyright © 2017 Pavel Kozlovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
